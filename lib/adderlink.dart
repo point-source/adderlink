9b80cb2b2e66992a84c8ff8bcbd3d87500db48a5
PointSource <47544779+point-source@users.noreply.github.com>
2022-06-16 10:34:42 -0400

    Init dart package

---
+++ lib/adderlink.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library adderlink;
+
+export 'src/adderlink_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
