701f974c1165e691940224ba903eb7bbf747b2d1
PointSource <47544779+point-source@users.noreply.github.com>
2022-06-17 14:32:24 -0400

    Update example

--- example/adderlink_example.dart
+++ example/adderlink_example.dart
@@ -1,6 +1,17 @@
 import 'package:adderlink/adderlink.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  var adderlink = Adderlink(ipAddress: '192.168.1.100');
+
+  // Login with your username and password
+  await adderlink.login(username: 'username', password: 'password');
+
+  // Get a list of all C-USB Extenders
+  var cUsbList = await adderlink.getAllCUsb();
+
+  // Print each extender's data to the console
+  cUsbList.body?.forEach(print);
+
+  // Logout so that the token does not stay active and clog up the auth buffer
+  await adderlink.logout();
 }
