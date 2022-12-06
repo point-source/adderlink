2689d11bfda95825b5641faa43e1a9c206d61e11
PointSource <47544779+point-source@users.noreply.github.com>
2022-12-06 12:20:02 -0800

    Add tests for auth and get requests

--- test/adderlink_test.dart
+++ test/adderlink_test.dart
@@ -8,10 +8,48 @@ void main() {
       baseUrl: "http://192.168.1.100",
       converter: AlifConverter(),
     );
-    var adderlink = AdderlinkService.create(chopper);
+
+    late AdderlinkService adderlink;
+    late String token;
 
     test('Init', () {
+      adderlink = AdderlinkService.create(chopper);
       expect(adderlink, isA<AdderlinkService>());
+    });
+
+    test('Login', () async {
+      final request = await adderlink.login(
+        username: '',
+        password: '',
+      );
+      expect(request.isSuccessful, isTrue);
+      expect(request.body?.isSuccessful, isTrue);
+      token = request.body?.body?.token ?? '';
+      expect(token, isNotEmpty);
+    });
+
+    test('Get devices', () async {
+      final request = await adderlink.getDevices(token: token);
+      expect(request.isSuccessful, isTrue);
+      expect(request.body?.isSuccessful, isTrue);
+    });
+
+    test('Get channels', () async {
+      final request = await adderlink.getChannels(token: token);
+      expect(request.isSuccessful, isTrue);
+      expect(request.body?.isSuccessful, isTrue);
+    });
+
+    test('Get C-USB devices', () async {
+      final request = await adderlink.getAllCUsb(token: token);
+      expect(request.isSuccessful, isTrue);
+      expect(request.body?.isSuccessful, isTrue);
+    });
+
+    test('Logout', () async {
+      final request = await adderlink.logout(token: token);
+      expect(request.isSuccessful, isTrue);
+      expect(request.body?.isSuccessful, isTrue);
     });
   });
 }
